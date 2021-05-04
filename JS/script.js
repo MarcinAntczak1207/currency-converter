0042cadccfc8b07848510121d896689e10074d30
Marcin Antczak <marcin.antczak12@gmail.com>
2021-05-04 12:59:25 +0200

    select currency added

--- JS/script.js
+++ JS/script.js
@@ -1,18 +1,41 @@
 let formElement = document.querySelector(".js-container__form");
 let resultElement = document.querySelector(".js-container__result");
 let amountElement = document.querySelector(".js-form__amount");
-let rateElement = document.querySelector(".js-form__rate");
+let currencyElement = document.querySelector(".js-form__currency");
+
+let rateEUR = 4.5523;
+let rateUSD = 3.7902;
+let rateGBP = 4.1415;
+let rateCHF = 5.2600;
 
 formElement.addEventListener("submit", (event) => {
     event.preventDefault();
 
-    let amount = amountElement.value;
-    let rate = rateElement.value;
+    let amount = +amountElement.value;
+    let currency = currencyElement.value;
 
-    let result = amount * rate;
+    let result;
 
-    resultElement.innerText = result;
-})
+    switch (currency) {
+        case "EUR":
+            result = amount / rateEUR;
+            break;
+
+        case "USD":
+            result = amount / rateGBP;
+            break;
+            
+        case "GBP":
+            result = amount / rateGBP;
+            break;
+
+        case "CHF":
+            result = amount / rateGBP;
+            break;            
+    }
+
+    resultElement.innerHTML = `<strong>${amount} ZŁ = ${result.toFixed(2)} ${currency}</strong>`;
+});
 
 
 
